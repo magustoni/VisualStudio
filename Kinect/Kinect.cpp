8851739847caf1c97309e84bc51ba1b7eda365a0
magustoni <m.agustoni@alumnos.upm.es>
2022-02-24 12:29:13 +0100

    Modos de funcionamiento

--- Kinect/Kinect.cpp
+++ Kinect/Kinect.cpp
@@ -1,12 +1,18 @@
 ﻿#include <assert.h>
 #include <iostream>
+#include <fstream>
 #include <k4a/k4a.hpp>
 #include <k4abt.hpp>
 
-#define FRAMES 100
-#define PRINT false 
+#define FRAMES 20 //Numero de frames a capturar
+#define MODE 1 //Modo de funcionamiento
+//0 -> Modo analisis manual en .txt
+//1- > Modo captura datos en .xls
+#define PRINT false //Salida (solo modo 0)
 //false -> Solo numero articulaciones captadas
 //true -> Numero y posiciones de cada una
+#define GPU false //Activar procesamiento en GPU
+
 using namespace std;
 
 //Variables globales
@@ -16,7 +22,7 @@ k4abt::tracker tracker; //Modulo "tracker"
 k4abt::frame body_frame; //Frame del cuerpo
 k4abt_body_t body; //Objeto cuerpo
 uint32_t num_bodies; //Numero de cuerpos detectados
-FILE* fichero; //Fichero de salida
+fstream fichero; //Fichero de salida
 
 //Conjunto de articulaciones de interes
 const vector<int> desired_joints = { 1,2,3,4,5,6,7,11,12,13,14,18,19,20,21,22,23,24,25,26 };
@@ -39,6 +45,18 @@ int main()
 		return 1;
 	}
 
+	if (MODE == 0) fichero << "Modo analisis manual" << endl << endl;
+	if (MODE == 1)
+	{
+		fichero << "Frame\t\t";
+		for (auto k : desired_joints) {
+			fichero << name(k) << " X\t";
+			fichero << name(k) << " Y\t";
+			fichero << name(k) << " Z\t";
+		}
+		fichero << endl;
+	}
+
 	//Bucle de captura
 	for (int frame = 0; frame < FRAMES; frame++)
 	{
@@ -52,59 +70,103 @@ int main()
 		//Numero de cuerpos detectados
 		num_bodies = body_frame.get_num_bodies();
 
-		fprintf(fichero, "Frame: %d", frame + 1);
-		if (num_bodies == 0) fprintf(fichero, "\n\t\t--------No se detectaron sujetos-------\n");
-		else
-			for (int i = 0; i < num_bodies; i++)
-			{
-				body = body_frame.get_body(i);
-				fprintf(fichero, " | ID: %d\n", body.id);
-				fprintf(fichero, "Captadas %d/20 articulaciones de interes\n\n", print_body_information(body));
-			}
+		if (MODE == 0) fichero << "Frame " << frame + 1 << endl;
+		if (MODE == 1) fichero << frame + 1 << "\t\t";
+
+		if (num_bodies == 0) {
+			if (MODE == 0)
+				fichero << "\t\t--------No se detectaron sujetos-------\n";
+		}
+		else {
+			body = body_frame.get_body(0);
+			if (MODE == 0) fichero << "Captadas " << print_body_information(body) << " / 20 articulaciones de interes" << endl << endl;
+			if (MODE == 1) print_body_information(body);
+		}
+
 	}
 	cout << "Fin del programa Kinect" << endl;
 
-	fclose(fichero);
-	system("Kinect.txt");
+	fichero.close();
+	if (MODE == 0) system("Kinect.txt");
+	if (MODE == 1) system("Kinect.xls");
 
 	return 0;
 }
 
 const char* name(int i) {
-	switch (i) {
-	case 0: return "Centro Cadera\t";
-	case 1: return "Cintura\t\t";
-	case 2: return "Esternon\t";
-	case 3: return "Cuello\t\t";
-	case 4: return "Clavicula Izq\t";
-	case 5: return "Hombro Izq\t";
-	case 6: return "Codo Izq\t";
-	case 7: return "Muneca Izq\t";
-	case 8: return "Mano Izq\t";
-	case 9: return "Palma Izq\t";
-	case 10: return "Pulgar Izq\t";
-	case 11: return "Clavicula Der\t";
-	case 12: return "Hombro Der\t";
-	case 13: return "Codo Der\t";
-	case 14: return "Muneca Der\t";
-	case 15: return "Mano Der\t";
-	case 16: return "Palma Der\t";
-	case 17: return "Pulgar Der\t";
-	case 18: return "Cadera Izq\t";
-	case 19: return "Rodilla Izq\t";
-	case 20: return "Tobillo Izq\t";
-	case 21: return "Pie Izq\t\t";
-	case 22: return "Cadera Der\t";
-	case 23: return "Rodilla Der\t";
-	case 24: return "Tobillo Der\t";
-	case 25: return "Pie Der\t\t";
-	case 26: return "Cabeza\t\t";
-	case 27: return "Nariz\t\t";
-	case 28: return "Ojo Izq\t\t";
-	case 29: return "Oreja Izq\t";
-	case 30: return "Ojo Der\t\t";
-	case 31: return "Oreja Der\t";
-	default: return "NONE\t\t";
+	if (MODE == 0) {
+		switch (i) {
+		case 0: return "Centro Cadera\t";
+		case 1: return "Cintura\t\t";
+		case 2: return "Esternon\t";
+		case 3: return "Cuello\t\t";
+		case 4: return "Clavicula Izq\t";
+		case 5: return "Hombro Izq\t";
+		case 6: return "Codo Izq\t";
+		case 7: return "Muneca Izq\t";
+		case 8: return "Mano Izq\t";
+		case 9: return "Palma Izq\t";
+		case 10: return "Pulgar Izq\t";
+		case 11: return "Clavicula Der\t";
+		case 12: return "Hombro Der\t";
+		case 13: return "Codo Der\t";
+		case 14: return "Muneca Der\t";
+		case 15: return "Mano Der\t";
+		case 16: return "Palma Der\t";
+		case 17: return "Pulgar Der\t";
+		case 18: return "Cadera Izq\t";
+		case 19: return "Rodilla Izq\t";
+		case 20: return "Tobillo Izq\t";
+		case 21: return "Pie Izq\t\t";
+		case 22: return "Cadera Der\t";
+		case 23: return "Rodilla Der\t";
+		case 24: return "Tobillo Der\t";
+		case 25: return "Pie Der\t\t";
+		case 26: return "Cabeza\t\t";
+		case 27: return "Nariz\t\t";
+		case 28: return "Ojo Izq\t\t";
+		case 29: return "Oreja Izq\t";
+		case 30: return "Ojo Der\t\t";
+		case 31: return "Oreja Der\t";
+		default: return "NONE\t\t";
+		}
+	}
+	if (MODE == 1) {
+		switch (i) {
+		case 0: return "Centro Cadera";
+		case 1: return "Cintura";
+		case 2: return "Esternon";
+		case 3: return "Cuello";
+		case 4: return "Clavicula Izq";
+		case 5: return "Hombro Izq";
+		case 6: return "Codo Izq";
+		case 7: return "Muneca Izq";
+		case 8: return "Mano Izq";
+		case 9: return "Palma Izq";
+		case 10: return "Pulgar Izq";
+		case 11: return "Clavicula Der";
+		case 12: return "Hombro Der";
+		case 13: return "Codo Der";
+		case 14: return "Muneca Der";
+		case 15: return "Mano Der";
+		case 16: return "Palma Der";
+		case 17: return "Pulgar Der";
+		case 18: return "Cadera Izq";
+		case 19: return "Rodilla Izq";
+		case 20: return "Tobillo Izq";
+		case 21: return "Pie Izq";
+		case 22: return "Cadera Der";
+		case 23: return "Rodilla Der";
+		case 24: return "Tobillo Der";
+		case 25: return "Pie Der";
+		case 26: return "Cabeza";
+		case 27: return "Nariz";
+		case 28: return "Ojo Izq";
+		case 29: return "Oreja Izq";
+		case 30: return "Ojo Der";
+		case 31: return "Oreja Der";
+		default: return "NONE";
+		}
 	}
 }
 bool belongs(int index, const vector<int> v) {
@@ -115,40 +177,58 @@ bool belongs(int index, const vector<int> v) {
 int print_body_information(k4abt_body_t body)
 {
 	int caught = 0; //Numero de articulaciones de interes captadas
-	for (int i = 0; i < (int)K4ABT_JOINT_COUNT; i++)
-	{
-		k4a_float3_t position = body.skeleton.joints[i].position;
 
-		if (body.skeleton.joints[i].confidence_level >= K4ABT_JOINT_CONFIDENCE_MEDIUM) //El nivel de confianza indica si la articulacion ha sido detectada
+	if (MODE == 0) {
+		for (int i = 0; i < (int)K4ABT_JOINT_COUNT; i++)
 		{
-			if (PRINT) //PRINT sirve para elegir si imprimir las posiciones o no
-				fprintf(fichero, "\t\t%s: Posicion[mm] ( %d, %d, %d ) \t", name(i), (int)position.v[0], (int)position.v[1], (int)position.v[2]);
-			if (belongs(i, desired_joints))
-				caught++;
-		}
-		else if (PRINT) fprintf(fichero, "\t\t%s: No detectada \t\t\t\t", name(i));
+			k4a_float3_t position = body.skeleton.joints[i].position;
+			if (body.skeleton.joints[i].confidence_level >= K4ABT_JOINT_CONFIDENCE_MEDIUM){ //El nivel de confianza indica si la articulacion ha sido detectada
+				if (PRINT) //PRINT sirve para elegir si imprimir las posiciones o no
+					fichero << "\t\t" << name(i) << ": Posicion[mm] ( " << (int)position.v[0] << ", " << (int)position.v[1] << ", " << (int)position.v[2] << " ) \t";
+				if (belongs(i, desired_joints))
+					caught++;
+			}
+			else if (PRINT) fichero << "\t\t" << name(i) << ": No detectada \t\t\t\t";
 
-		if (PRINT)
-		{
-			if (belongs(i, desired_joints))
-				fprintf(fichero, "<Deseada>\n");
-			else
-				fprintf(fichero, "\n");
+			if (PRINT) {
+				if (belongs(i, desired_joints))
+					fichero << "<Deseada>" << endl;
+				else
+					fichero << endl;
+			}
 		}
 	}
+	if (MODE == 1) {
+		for (int i = 0; i < (int)K4ABT_JOINT_COUNT; i++)
+		{
+			k4a_float3_t position = body.skeleton.joints[i].position;
+			if (belongs(i, desired_joints))	{
+				if (body.skeleton.joints[i].confidence_level >= K4ABT_JOINT_CONFIDENCE_MEDIUM) //El nivel de confianza indica si la articulacion ha sido detectada
+					fichero << position.v[0] << "\t" << position.v[1] << "\t" << position.v[2] << "\t";
+				else fichero << 0 << "\t" << 0 << "\t" << 0 << "\t";
+			}
+		}
+		fichero << endl;
+	}
 	return caught;
-} 
+}
 void init() {
 	//Configuracion de la camara
 	k4a_device_configuration_t device_config = K4A_DEVICE_CONFIG_INIT_DISABLE_ALL;
 	device_config.depth_mode = K4A_DEPTH_MODE_NFOV_2X2BINNED;
 	device = k4a::device::open(0);
 	device.start_cameras(&device_config);
-	k4a::calibration sensor_calibration = device.get_calibration(device_config.depth_mode, device_config.color_resolution);
 
 	//Inicializacion del objeto "tracker"
-	tracker = k4abt::tracker::create(sensor_calibration);
+	k4a::calibration sensor_calibration = device.get_calibration(device_config.depth_mode, device_config.color_resolution);
+	if (!GPU) {
+		k4abt_tracker_configuration_t tracker_config;
+		tracker_config.processing_mode = K4ABT_TRACKER_PROCESSING_MODE_CPU;
+		tracker = k4abt::tracker::create(sensor_calibration, tracker_config);
+	}
+	else tracker = k4abt::tracker::create(sensor_calibration);
 
 	//Apertura del fichero de salida
-	fichero = fopen("Kinect.txt", "w");
+	if (MODE == 0)	fichero.open("Kinect.txt", fstream::out);
+	if (MODE == 1)	fichero.open("Kinect.xls", fstream::out);
 }
--- Nuitrack/Nuitrack.cpp
+++ Nuitrack/Nuitrack.cpp
@@ -2,22 +2,27 @@
 #include <signal.h>
 #include <iomanip>
 #include <iostream>
-#include <Windows.h>
+#include <fstream>
+
 
 #define FRAMES 100 //Numero de frames a capturar
-#define PRINT false 
+#define MODE 0 //Modo de funcionamiento
+//0 -> Modo analisis manual en .txt
+//1- > Modo captura datos en .xls
+#define PRINT false //Salida (solo modo 0)
 //false -> Solo numero articulaciones captadas
 //true -> Numero y posiciones de cada una
+
 using namespace tdv::nuitrack;
 using namespace std;
 
 //Variables globales
-FILE* fichero;
-SkeletonTracker::Ptr skeletonTracker;
+fstream fichero; //Fichero de salida
+SkeletonTracker::Ptr skeletonTracker; //Modulo "tracker"
 
 //Conjunto de articulaciones de interes
-const vector<JointType> desired_joints = { JOINT_HEAD, JOINT_NECK, JOINT_LEFT_COLLAR, JOINT_RIGHT_COLLAR, JOINT_TORSO, JOINT_WAIST, JOINT_LEFT_SHOULDER,
-JOINT_LEFT_ELBOW, JOINT_LEFT_WRIST, JOINT_RIGHT_SHOULDER, JOINT_RIGHT_ELBOW,  JOINT_RIGHT_WRIST, JOINT_LEFT_HIP,
+const vector<JointType> desired_joints = { JOINT_HEAD, JOINT_NECK, JOINT_TORSO, JOINT_WAIST, JOINT_LEFT_COLLAR, JOINT_LEFT_SHOULDER, 
+JOINT_LEFT_ELBOW, JOINT_LEFT_WRIST, JOINT_RIGHT_COLLAR, JOINT_RIGHT_SHOULDER, JOINT_RIGHT_ELBOW,  JOINT_RIGHT_WRIST, JOINT_LEFT_HIP,
 JOINT_LEFT_KNEE, JOINT_LEFT_ANKLE, JOINT_LEFT_FOOT, JOINT_RIGHT_HIP, JOINT_RIGHT_KNEE, JOINT_RIGHT_ANKLE, JOINT_RIGHT_FOOT };
 
 //Funciones
@@ -37,18 +42,24 @@ int main(int argc, char* argv[])
 		return 1;
 	}
 
-	cout << "La captura comenzara en 3..." << endl;
-	Sleep(1000);
-	cout << "2..." << endl;
-	Sleep(1000);
-	cout << "1..." << endl;
-	Sleep(1000);
+	if (MODE == 0) fichero << "Modo analisis manual" << endl << endl;
+	if (MODE == 1)
+	{
+		fichero << "Frame\t\t";
+		for (auto k : desired_joints) {
+			fichero << name(k) << " X\t";
+			fichero << name(k) << " Y\t";
+			fichero << name(k) << " Z\t";
+		}
+		fichero << endl;
+	}
 
 	//Bucle de captura
 	for (int frame = 0; frame < FRAMES; frame++)
 	{
 		cout << "Procesando frame " << frame + 1 << endl;
-		fprintf(fichero, "Frame %d\n", frame + 1);
+		if (MODE == 0) fichero << "Frame " << frame + 1 << endl;
+		if (MODE == 1) fichero << frame + 1 << "\t\t";
 		//Esperar datos
 		try { Nuitrack::waitUpdate(skeletonTracker); }
 		catch (LicenseNotAcquiredException& e)
@@ -61,6 +72,7 @@ int main(int argc, char* argv[])
 			cerr << "Nuitrack update failed (ExceptionType: " << e.type() << ")" << endl;
 			return 1;
 		}
+		if (MODE == 1) fichero << endl;
 	}
 
 	//Liberar Nuitrack
@@ -72,40 +84,72 @@ int main(int argc, char* argv[])
 	}
 
 	cout << "Fin del programa Nuitrack" << endl;
-	fclose(fichero);
-	system("Nuitrack.txt");
+	fichero.close();
+	if (MODE == 0) system("Nuitrack.txt");
+	if (MODE == 1) system("Nuitrack.xls");
 
 	return 0;
 }
 
 const char* name(JointType joint)
 {
-	switch (joint) {
-	case JOINT_HEAD: return "Cabeza\t\t";
-	case JOINT_LEFT_ANKLE: return "Tobillo Izq\t";
-	case JOINT_LEFT_COLLAR: return "Clavicula Izq\t";
-	case JOINT_LEFT_ELBOW: return "Codo Izq\t";
-	case JOINT_LEFT_FINGERTIP: return "Dedo Izq\t";
-	case JOINT_LEFT_FOOT: return "Pie Izq\t\t";
-	case JOINT_LEFT_HAND: return "Mano Izq\t";
-	case JOINT_LEFT_HIP: return "Cadera Izq\t";
-	case JOINT_LEFT_KNEE: return "Rodilla Izq\t";
-	case JOINT_LEFT_SHOULDER: return "Hombro Izq\t";
-	case JOINT_LEFT_WRIST: return "Muñeca Izq\t";
-	case JOINT_NECK: return "Cuello\t\t";
-	case JOINT_TORSO: return "Esternon\t";
-	case JOINT_WAIST: return "Cintura\t\t";
-	case JOINT_RIGHT_ANKLE: return "Tobillo Der\t";
-	case JOINT_RIGHT_COLLAR: return "Clavicula Der\t";
-	case JOINT_RIGHT_ELBOW: return "Codo Der\t";
-	case JOINT_RIGHT_FINGERTIP: return "Dedo Der\t";
-	case JOINT_RIGHT_FOOT: return "Pie Der\t\t";
-	case JOINT_RIGHT_HAND: return "Mano Der\t";
-	case JOINT_RIGHT_HIP: return "Cadera Der\t";
-	case JOINT_RIGHT_KNEE: return "Rodilla Der\t";
-	case JOINT_RIGHT_SHOULDER: return "Hombro Der\t";
-	case JOINT_RIGHT_WRIST: return "Muñeca Der\t";
-	default: return "NONE\t\t";
+	if (MODE == 0) {
+		switch (joint) {
+		case JOINT_HEAD: return "Cabeza\t\t";
+		case JOINT_LEFT_ANKLE: return "Tobillo Izq\t";
+		case JOINT_LEFT_COLLAR: return "Clavicula Izq\t";
+		case JOINT_LEFT_ELBOW: return "Codo Izq\t";
+		case JOINT_LEFT_FINGERTIP: return "Dedo Izq\t";
+		case JOINT_LEFT_FOOT: return "Pie Izq\t\t";
+		case JOINT_LEFT_HAND: return "Mano Izq\t";
+		case JOINT_LEFT_HIP: return "Cadera Izq\t";
+		case JOINT_LEFT_KNEE: return "Rodilla Izq\t";
+		case JOINT_LEFT_SHOULDER: return "Hombro Izq\t";
+		case JOINT_LEFT_WRIST: return "Muñeca Izq\t";
+		case JOINT_NECK: return "Cuello\t\t";
+		case JOINT_TORSO: return "Esternon\t";
+		case JOINT_WAIST: return "Cintura\t\t";
+		case JOINT_RIGHT_ANKLE: return "Tobillo Der\t";
+		case JOINT_RIGHT_COLLAR: return "Clavicula Der\t";
+		case JOINT_RIGHT_ELBOW: return "Codo Der\t";
+		case JOINT_RIGHT_FINGERTIP: return "Dedo Der\t";
+		case JOINT_RIGHT_FOOT: return "Pie Der\t\t";
+		case JOINT_RIGHT_HAND: return "Mano Der\t";
+		case JOINT_RIGHT_HIP: return "Cadera Der\t";
+		case JOINT_RIGHT_KNEE: return "Rodilla Der\t";
+		case JOINT_RIGHT_SHOULDER: return "Hombro Der\t";
+		case JOINT_RIGHT_WRIST: return "Muñeca Der\t";
+		default: return "NONE\t\t";
+		}
+	}
+	if (MODE == 1) {
+		switch (joint) {
+		case JOINT_HEAD: return "Cabeza";
+		case JOINT_LEFT_ANKLE: return "Tobillo Izq";
+		case JOINT_LEFT_COLLAR: return "Clavicula Izq";
+		case JOINT_LEFT_ELBOW: return "Codo Izq";
+		case JOINT_LEFT_FINGERTIP: return "Dedo Izq";
+		case JOINT_LEFT_FOOT: return "Pie Izq";
+		case JOINT_LEFT_HAND: return "Mano Izq";
+		case JOINT_LEFT_HIP: return "Cadera Izq";
+		case JOINT_LEFT_KNEE: return "Rodilla Izq";
+		case JOINT_LEFT_SHOULDER: return "Hombro Izq";
+		case JOINT_LEFT_WRIST: return "Muñeca Izq";
+		case JOINT_NECK: return "Cuello";
+		case JOINT_TORSO: return "Esternon";
+		case JOINT_WAIST: return "Cintura";
+		case JOINT_RIGHT_ANKLE: return "Tobillo Der";
+		case JOINT_RIGHT_COLLAR: return "Clavicula Der";
+		case JOINT_RIGHT_ELBOW: return "Codo Der";
+		case JOINT_RIGHT_FINGERTIP: return "Dedo Der";
+		case JOINT_RIGHT_FOOT: return "Pie Der";
+		case JOINT_RIGHT_HAND: return "Mano Der";
+		case JOINT_RIGHT_HIP: return "Cadera Der";
+		case JOINT_RIGHT_KNEE: return "Rodilla Der";
+		case JOINT_RIGHT_SHOULDER: return "Hombro Der";
+		case JOINT_RIGHT_WRIST: return "Muñeca Der";
+		default: return "NONE";
+		}
 	}
 }
 void onSkeletonUpdate(SkeletonData::Ptr skeletonData)
@@ -121,7 +165,7 @@ void onSkeletonUpdate(SkeletonData::Ptr skeletonData)
 	//No hay esqueletos
 	if (userSkeletons.empty())
 	{
-		fprintf(fichero, "\t\t--------No se detectaron sujetos-------\n");
+		if(MODE == 0) fichero << "\t\t--------No se detectaron sujetos-------\n";
 		return;
 	}
 
@@ -129,27 +173,34 @@ void onSkeletonUpdate(SkeletonData::Ptr skeletonData)
 	int caught = 0;
 	auto joints = userSkeletons[0].joints;
 
-	for (int i = 0; i < joints.size(); i++)
-	{
-		if (joints[i].confidence >= 0.5) //El nivel de confianza indica si la articulacion ha sido detectada
+	if (MODE == 0) {
+		for (int i = 0; i < joints.size(); i++)
 		{
-			if (PRINT) //PRINT sirve para elegir si imprimir las posiciones o no
-				fprintf(fichero, "\t\t%s: Posicion[mm] ( %d, %d, %d ) \t", name(joints[i].type), (int)joints[i].real.x, (int)joints[i].real.y, (int)joints[i].real.z);
-			if (belongs(i, desired_joints))
-				caught++;
-		}
-		else if (PRINT && joints[i].type != JOINT_NONE) fprintf(fichero, "\t\t%s: No detectada \t\t\t\t", name(joints[i].type));
+			if (joints[i].confidence >= 0.5) //El nivel de confianza indica si la articulacion ha sido detectada
+			{
+				if (PRINT) //PRINT sirve para elegir si imprimir las posiciones o no
+					fichero << "\t\t" << name(joints[i].type) << ": Posicion[mm] ( " << (int)joints[i].real.x << ", " << (int)joints[i].real.y << ", " << (int)joints[i].real.z << " ) \t";
+				if (belongs(i, desired_joints))
+					caught++;
+			}
+			else if (PRINT && joints[i].type != JOINT_NONE) fichero << "\t\t" << name(joints[i].type) << ": No detectada \t\t\t\t";
 
-		if (PRINT)
-		{
-			if (belongs(i, desired_joints))
-				fprintf(fichero, "<Deseada>\n");
-			else
-				fprintf(fichero, "\n");
+			if (PRINT)
+			{
+				if (belongs(i, desired_joints))
+					fichero << "<Deseada>" << endl;
+				else
+					fichero << endl;
+			}
 		}
+
+		fichero << "Captadas " << caught << " / 20 articulaciones de interes" << endl << endl;
 	}
-
-	fprintf(fichero, "Captadas %d/20 articulaciones de interes\n\n", caught);
+	if (MODE == 1) {
+		for (int i = 0; i < joints.size(); i++)
+			if (belongs(i, desired_joints))
+				fichero << joints[i].real.x << "\t" << joints[i].real.y << "\t" << joints[i].real.z << "\t";
+	}
 }
 bool belongs(int index, const vector<JointType> v) {
 	for (int i = 0; i < v.size(); i++)
@@ -159,7 +210,8 @@ bool belongs(int index, const vector<JointType> v) {
 void init()
 {
 	//Apertura del fichero de salida
-	fichero = fopen("Nuitrack.txt", "w");
+	if (MODE == 0)	fichero.open("Nuitrack.txt", fstream::out);
+	if (MODE == 1)	fichero.open("Nuitrack.xls", fstream::out);
 
 	//Configuracion Nuitrack
 	Nuitrack::init();
